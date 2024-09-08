8f9f33fd9272118d79e0a86aa4ebed5a30445921
mkusaka <hinoshita1992@gmail.com>
2024-09-09 02:06:26 +0900

    jsdebug

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-export function dbg<T>(value: T) {
+export function dbg<T>(value: T, ...optionalParams: any[]) {
   const error = new Error();
   const stack = error.stack?.split("\n")[2];
   const [, filePath, lineNumber] = stack?.match(/\((.*):(\d+):\d+\)/) || [];
 
-  console.error(`[${filePath}:${lineNumber}]`, value);
+  console.error(`[${filePath}:${lineNumber}]`, value, ...optionalParams);
   return value;
 }
