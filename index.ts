8fea7a4acc8f99d5f12879c718ae575c9bafc9a6
mkusaka <hinoshita1992@gmail.com>
2024-09-09 01:15:39 +0900

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 export function dbg<T>(value: T) {
   const error = new Error();
-  const stack = error.stack.split('\n')[2];
-  const [, filePath, lineNumber] = stack.match(/\((.*):(\d+):\d+\)/) || [];
+  const stack = error.stack?.split("\n")[2];
+  const [, filePath, lineNumber] = stack?.match(/\((.*):(\d+):\d+\)/) || [];
 
   console.error(`[${filePath}:${lineNumber}]`, value);
   return value;
