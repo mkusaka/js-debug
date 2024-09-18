a1db585041ab35b531b7847d2bec8fb09ef6f110
mkusaka <hinoshita1992@gmail.com>
2024-09-19 01:29:19 +0900

    use inspect

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
+import { inspect } from "util";
 export function dbg<T>(value: T, { depth }: { depth?: number } = {}): T {
   const error = new Error();
   const stack = error.stack?.split("\n")[2];
   const [, filePath, lineNumber, colNumber] = stack?.match(/\((.*):(\d+):(\d+)\)/) || [];
 
-  console.error(`[${filePath}:${lineNumber}:${colNumber}]`, value);
+  console.error(`[${filePath}:${lineNumber}:${colNumber}]`, inspect(value, { showHidden: true, depth, colors: true }));
   return value;
 }
